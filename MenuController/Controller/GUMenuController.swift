0e7ee9ec2dc17980e88169b6c644ba7d3c624e85
Eran Guttentag <>
2018-07-10 14:30:20 +0300

    description text

--- MenuController/Controller/GUMenuController.swift
+++ MenuController/Controller/GUMenuController.swift
@@ -21,7 +21,10 @@ class GUMenuController: UIView {
     @IBOutlet private weak var contentView: UIView!
     @IBOutlet private weak var itemsTableView: UITableView!
     @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var contentButton: UIButton!
+    @IBOutlet private weak var contextButton: UIButton!
+    @IBOutlet private weak var contextScrollView: UIScrollView!
+    @IBOutlet private weak var subtitleTextView: UITextView!
+    
     private var widthConstraint: NSLayoutConstraint!
     private var dataSource = [MenuItem]()
     
@@ -58,15 +61,25 @@ class GUMenuController: UIView {
         }
     }
     
-//    var subTitle: NSAttributedString? {
-//        get {
-//            return self.subTitleLabel.attributedText
-//        }
-//        
-//        set {
-//            self.subTitleLabel.attributedText = newValue
-//        }
-//    }
+    var attributedSubtitle: NSAttributedString! {
+        get {
+            return self.subtitleTextView.attributedText
+        }
+        
+        set {
+            self.subtitleTextView.attributedText = newValue
+        }
+    }
+    
+    var subtitle: String! {
+        get {
+            return self.subtitleTextView.text
+        }
+        
+        set {
+            self.subtitleTextView.text = newValue
+        }
+    }
     
     var data: [MenuItem] {
         get {
@@ -101,7 +114,7 @@ class GUMenuController: UIView {
         super.awakeFromNib()
         
         self.backgroundColor = UIColor.clear
-        self.contentView.backgroundColor = UIColor.clear
+        self.contentView.backgroundColor = UIColor.black.withAlphaComponent(0.2)
         self.itemsTableView.backgroundColor = UIColor.clear
         
         self.itemsTableView.register(MenuItemTableViewCell.self, forCellReuseIdentifier: MenuItemTableViewCell.identifier)
@@ -113,10 +126,21 @@ class GUMenuController: UIView {
         self.itemsTableView.showsVerticalScrollIndicator = false
         
         self.titleLabel.textColor = UIColor.white
-        self.contentButton.setTitleColor(UIColor.white, for: .normal)
-        self.contentButton.setTitleColor(UIColor.gray, for: .highlighted)
-        self.contentButton.addTarget(self, action: #selector(GUMenuController.changeContext), for: .touchUpInside)
+        
+        self.subtitleTextView.textColor = UIColor.white
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+//        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+//        gradientLayer.colors = [UIColor.white.withAlphaComponent(0.4).cgColor, UIColor.white.withAlphaComponent(0.2).cgColor, UIColor.white.withAlphaComponent(0.1).cgColor, UIColor.white.withAlphaComponent(0).cgColor]
+//        gradientLayer.frame = self.subtitleTextView.bounds
+//        self.subtitleTextView.layer.addSublayer(gradientLayer)
+        
+        self.contextButton.setTitleColor(UIColor.white, for: .normal)
+        self.contextButton.setTitleColor(UIColor.gray, for: .highlighted)
+        self.contextButton.addTarget(self, action: #selector(GUMenuController.changeContext), for: .touchUpInside)
         self.setContext(.list)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(GUMenuController.orientationChanged), name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
     }
 }
 
@@ -147,14 +171,22 @@ private extension GUMenuController {
     func setContext(_ toContext: ViewContext) {
         switch toContext {
         case .description:
-            self.contentButton.setTitle("Show Available Items", for: .normal)
-            self.contentButton.setTitle("Show Available Items", for: .highlighted)
+            self.contextButton.setTitle("Show Available Items", for: .normal)
+            self.contextButton.setTitle("Show Available Items", for: .highlighted)
+            self.contextScrollView.setContentOffset(CGPoint(x: 0, y: self.contextScrollView.contentSize.height - self.contextScrollView.frame.height), animated: true)
+            self.subtitleTextView.setContentOffset(CGPoint.zero, animated: false)
             self.context = .description
         case .list:
-            self.contentButton.setTitle("Show Room Description", for: .normal)
-            self.contentButton.setTitle("Show Room Description", for: .highlighted)
+            self.contextButton.setTitle("Show Room Description", for: .normal)
+            self.contextButton.setTitle("Show Room Description", for: .highlighted)
+            self.contextScrollView.setContentOffset(CGPoint.zero, animated: true)
+
             self.context = .list
         }
+    }
+    
+    @objc func orientationChanged() {
+        self.setContext(self.context)
     }
 }
 
